d4ff6cc246e7baea34a5dcbbc25a04b19fb4906a
amr1n <52592047+amr1n@users.noreply.github.com>
2021-03-14 11:07:10 +0600

    organaized the code wrapped all the functions in mudules.

--- script.js
+++ script.js
@@ -1,98 +1,109 @@
+let dom = (() => {
+	let board = document.querySelectorAll(".cell");
+	let htmlBoard = document.querySelector(".gameboard");
+	let playAgain = document.querySelector(".playAgain");
+	let message = document.querySelector(".message");
 
-let Player = (name) => {
-	this.name = name;
 	return {
-		name
+		board,
+		htmlBoard,
+		playAgain,
+		message,
+	};
+})();
+
+let Players = (() => {
+	let playerX = "X";
+	let playerO = "O";
+	let playerXId = [];
+	let playerOId = [];
+
+	return {
+		playerX, 
+		playerO,
+		playerOId,
+		playerXId,
+	};
+	
+})();
+
+let currentPlayer = Players.playerX;
+function activePlayer() {
+	if (currentPlayer == Players.playerX) {
+		dom.message.textContent = `Player ${currentPlayer}'s turn.`;
+		currentPlayer = Players.playerO;
+	}else {
+		dom.message.textContent = `Player ${currentPlayer}'s turn.`;
+		currentPlayer = Players.playerX; 
 	}
 }
 
-let gameboard = (() =>{
+let gameboard = (() => {
 	let board = ["", "", "", "", "", "", "", "", ""];
-	let grid = document.querySelectorAll(".cell");
-	let htmlBoard = document.querySelector(".gameboard");
-	let message = document.querySelector(".message");
-	let playAgain = document.querySelector(".playAgain");
-
-	let PlayerX = Player("x");
-	let PlayerO = Player("O");
-	let currentPlayer = PlayerO;
-	let PlayerXId = [];
-	let PlayerOId = [];
-
-	function activePalyer() {
-		if (currentPlayer == PlayerO) {
-			message.textContent = `Player ${currentPlayer.name}'s turn`;
-			currentPlayer = PlayerX;
-		}else {
-			message.textContent = `Player ${currentPlayer.name}'s turn`;
-			currentPlayer = PlayerO;
-		}
-	}
-
-	//render the content of the board on webpage;
-	grid.forEach((cell) => {
-		cell.addEventListener("click", ()=> {
+	dom.board.forEach(cell => {
+		cell.addEventListener("click", () => {
 			if (cell.textContent == "") {
-				activePalyer();
-				board[cell.id] = currentPlayer.name;
+				activePlayer();
+				board[cell.id] = currentPlayer;
 				cell.textContent = board[cell.id];
 
-				if (currentPlayer == PlayerX) {
-					PlayerXId.push(Number(cell.id));
+				if (currentPlayer == Players.playerX) {
+					Players.playerXId.push(Number(cell.id));
 				}else {
-					PlayerOId.push(Number(cell.id));
+					Players.playerOId.push(Number(cell.id));
 				}
-			}
+			};
 
-			winnerChecker();
-		});
-	}); 
-
-	function winnerChecker() {
-		
-		let winningSequences = [
-		  [0, 1, 2],  
-	      [3, 4, 5],
-	      [6, 7, 8],
-	      [0, 3, 6],
-	      [1, 4, 7],
-	      [2, 5, 8],
-	      [0, 4, 8],
-	      [2, 4, 6],   
-      ];
-
-      	function checker(arr1, arr2) {
-			return arr1.every(elem => arr2.includes(elem));
-		}
-
-		for (i = 0; i < winningSequences.length; i++) {
-			let isWon;			
-			if (PlayerXId.length >= 3 || PlayerOId.length >= 3) {
-				isWon = checker(winningSequences[i], PlayerXId) || checker(winningSequences[i], PlayerOId);
-
-				if (isWon) {	
-					message.textContent = `Player ${currentPlayer.name} is the Winner`;
-					htmlBoard.classList.add("removeBoard");
-				}else if (!isWon && (PlayerXId.length + PlayerOId.length) == 9) {
-					message.textContent = "it was a draw";
-				}
-			}
-		}
-	}
-
-	playAgain.addEventListener("click", () => {
-		board = ["", "", "", "", "", "", "", "", ""];
-		currentPlayer = PlayerO;
-		PlayerXId = [];
-		PlayerOId = [];
-		htmlBoard.classList.remove("removeBoard");
-
-		grid.forEach(cell => {
-			cell.textContent = "";
+			chooseWinner();
+			
 		});
 	});
 
-})(); 
- 
+	return {
+		board,
+	}
+})();
 
+let chooseWinner = () => {
+	let winningSequences = [
+	  [0, 1, 2],  
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6], 
+	];
 
+	function checker(sequence, playerId) {
+		return sequence.every(elem => playerId.includes(elem));
+	}
+
+	for (i = 0; i < winningSequences.length; i++) {
+		let isWon;
+		if (Players.playerXId.length >= 3 || Players.playerOId.length >= 3) {
+			isWon = checker(winningSequences[i], Players.playerXId) || checker(winningSequences[i], Players.playerOId);
+
+			if (isWon) {
+				dom.message.textContent = `Player ${currentPlayer} is the Winner`;
+				dom.htmlBoard.classList.add("removeBoard");
+			}else if (!isWon && (Players.playerXId.length + Players.playerOId.length) == 9) {
+				dom.message.textContent = `It was a draw.`;
+			}
+		}
+	}
+
+};
+
+dom.playAgain.addEventListener("click", ()=> {
+	gameboard.board = ["", "", "", "", "", "", "", "", ""];
+	currentPlayer = Players.playerX;
+	Players.playerXId = [];
+	Players.playerOId = [];
+	dom.htmlBoard.classList.remove("removeBoard");
+
+	dom.board.forEach(cell => {
+		cell.textContent = "";
+	});
+});
